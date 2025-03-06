bf9fb171234f212ac283ad04d60bcd6a8079091c
hypnogaj <ruyadmrcii@gmail.com>
2025-03-06 15:56:18 +0300

    MerhabaDunya

---
+++ MerhabaDunya/MerhabaDunya/MerhabaDunya/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Merhaba, Dünya!"); 
+    }
+}
