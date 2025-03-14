c0bb872a458be6e0c2c25c8f50b540a78193d187
hypnogaj <ruyadmrcii@gmail.com>
2025-03-14 16:47:51 +0300

    HesapMakinesi

---
+++ BasitHesapMakinesi/HesapMakinesi/HesapMakinesi/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HesapMakinesi hesapMakinesi = new HesapMakinesi();
+            hesapMakinesi.Hesapla();
+
+            Console.WriteLine("Devam etmek için bir tuşa basın...");
+            Console.Read(); 
+        }
+    }
+}
