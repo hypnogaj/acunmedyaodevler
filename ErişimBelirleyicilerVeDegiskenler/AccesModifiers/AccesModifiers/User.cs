0bfd13cfd3ed837180215f6fcccace8ec2d8bb6f
hypnogaj <ruyadmrcii@gmail.com>
2025-03-20 12:30:27 +0300

    ErisimBelirleyicilerVeDegiskenler

---
+++ ErişimBelirleyicilerVeDegiskenler/AccesModifiers/AccesModifiers/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesModifiers
+{
+    public class User
+    {
+        private string adSoyad;
+        private int yas;
+
+        public string Email { get; set; }
+
+        // Getter ve Setter metodları
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string adSoyad)
+        {
+            this.adSoyad = adSoyad;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int yas)
+        {
+            if (yas > 0)
+            {
+                this.yas = yas;
+            }
+            else
+            {
+                Console.WriteLine("Yaş negatif olamaz!");
+            }
+        }
+
+        // Bilgileri gösteren metot
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}\nYaş: {yas}\nEmail: {Email}");
+        }
+    }
+}
---
+++ ErişimBelirleyicilerVeDegiskenler/ErişimBelirleyicilerVeDegiskenler/AccesModifiers/AccesModifiers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace KullaniciYonetimi
+{
+    public class User
+    {
+        private string adSoyad;
+        private int yas;
+        public string Email { get; set; }
+
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string ad)
+        {
+            adSoyad = ad;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int age)
+        {
+            if (age > 0)
+                yas = age;
+            else
+                Console.WriteLine("Yaş negatif olamaz!");
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}\nYaş: {yas}\nEmail: {Email}");
+        }
+    }
+}
+
+
+namespace ProgramUygulamasi
+{
+    using KullaniciYonetimi;
+
+    class Program
+    {
+        static void Main()
+        {
+            User kullanici = new User();
+            kullanici.SetAdSoyad("Rüya Sena Demirci");
+            kullanici.SetYas(21);
+            kullanici.Email = "ruyadmrcii@gmail.com";
+            kullanici.BilgileriGoster();
+        }
+    }
+}
