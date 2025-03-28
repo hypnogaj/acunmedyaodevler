03514f87c0c18ceca1f44fd13d238d406ddf1abb
hypnogaj <ruyadmrcii@gmail.com>
2025-03-29 00:18:17 +0300

    GenericListOdev

---
+++ GenericList/GenericList/GenericList/Araba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Araba
+    {
+        public Marka Marka { get; set; }
+        public Model Model { get; set; }
+        public double BenzinHarcamasi { get; set; }
+        public double ToplamMesafe { get; set; }
+
+        public Araba(Marka marka, Model model, double benzinHarcamasi, double toplamMesafe)
+        {
+            Marka = marka;
+            Model = model;
+            BenzinHarcamasi = benzinHarcamasi;
+            ToplamMesafe = toplamMesafe;
+        }
+
+        public double Hesapla()
+        {
+            return (BenzinHarcamasi / 100) * ToplamMesafe;
+        }
+
+        public void Yazdir()
+        {
+            Console.WriteLine($"Marka: {Marka.Ad}, Model: {Model.Ad}, 100 km'de Yaktığı Yakıt: {BenzinHarcamasi} litre, Toplam Yakıt Tüketimi: {Hesapla()} litre");
+        }
+    }
+}
---
+++ GenericList/GenericList/GenericList/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
