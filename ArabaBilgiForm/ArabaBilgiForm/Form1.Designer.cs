391a3f304eb615f2417950d5b6435fac5c2aab7b
hypnogaj <ruyadmrcii@gmail.com>
2025-03-29 02:46:26 +0300

    ArabaBilgi

---
+++ ArabaBilgiForm/ArabaBilgiForm/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ArabaBilgiForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Markalbl = new System.Windows.Forms.Label();
+            this.Modellbl = new System.Windows.Forms.Label();
+            this.Renklbl = new System.Windows.Forms.Label();
+            this.KapıSayısılbl = new System.Windows.Forms.Label();
+            this.PencereSayısılbl = new System.Windows.Forms.Label();
+            this.Yakıtlbl = new System.Windows.Forms.Label();
+            this.btnBilgileriGöster = new System.Windows.Forms.Button();
+            this.MarkatextBox = new System.Windows.Forms.TextBox();
+            this.ModeltextBox = new System.Windows.Forms.TextBox();
+            this.RenktextBox = new System.Windows.Forms.TextBox();
+            this.KapıSayısıtextBox = new System.Windows.Forms.TextBox();
+            this.PencereSayısıtextBox = new System.Windows.Forms.TextBox();
+            this.YakıttextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Markalbl
+            // 
+            this.Markalbl.AutoSize = true;
+            this.Markalbl.Location = new System.Drawing.Point(53, 89);
+            this.Markalbl.Name = "Markalbl";
+            this.Markalbl.Size = new System.Drawing.Size(48, 16);
+            this.Markalbl.TabIndex = 0;
+            this.Markalbl.Text = "Marka:";
+            this.Markalbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Modellbl
+            // 
+            this.Modellbl.AutoSize = true;
+            this.Modellbl.Location = new System.Drawing.Point(53, 133);
+            this.Modellbl.Name = "Modellbl";
+            this.Modellbl.Size = new System.Drawing.Size(48, 16);
+            this.Modellbl.TabIndex = 1;
+            this.Modellbl.Text = "Model:";
+            // 
+            // Renklbl
+            // 
+            this.Renklbl.AutoSize = true;
+            this.Renklbl.Location = new System.Drawing.Point(53, 180);
+            this.Renklbl.Name = "Renklbl";
+            this.Renklbl.Size = new System.Drawing.Size(42, 16);
+            this.Renklbl.TabIndex = 2;
+            this.Renklbl.Text = "Renk:";
+            // 
+            // KapıSayısılbl
+            // 
+            this.KapıSayısılbl.AutoSize = true;
+            this.KapıSayısılbl.Location = new System.Drawing.Point(53, 226);
+            this.KapıSayısılbl.Name = "KapıSayısılbl";
+            this.KapıSayısılbl.Size = new System.Drawing.Size(77, 16);
+            this.KapıSayısılbl.TabIndex = 3;
+            this.KapıSayısılbl.Text = "Kapı Sayısı:";
+            // 
+            // PencereSayısılbl
+            // 
+            this.PencereSayısılbl.AutoSize = true;
+            this.PencereSayısılbl.Location = new System.Drawing.Point(53, 272);
+            this.PencereSayısılbl.Name = "PencereSayısılbl";
+            this.PencereSayısılbl.Size = new System.Drawing.Size(101, 16);
+            this.PencereSayısılbl.TabIndex = 4;
+            this.PencereSayısılbl.Text = "Pencere Sayısı:";
+            // 
+            // Yakıtlbl
+            // 
+            this.Yakıtlbl.AutoSize = true;
+            this.Yakıtlbl.Location = new System.Drawing.Point(53, 313);
+            this.Yakıtlbl.Name = "Yakıtlbl";
+            this.Yakıtlbl.Size = new System.Drawing.Size(184, 16);
+            this.Yakıtlbl.TabIndex = 5;
+            this.Yakıtlbl.Text = "100 km\'de Yaktığı Yakıt (Litre):";
+            // 
+            // btnBilgileriGöster
+            // 
+            this.btnBilgileriGöster.Location = new System.Drawing.Point(577, 387);
+            this.btnBilgileriGöster.Name = "btnBilgileriGöster";
+            this.btnBilgileriGöster.Size = new System.Drawing.Size(196, 49);
+            this.btnBilgileriGöster.TabIndex = 6;
+            this.btnBilgileriGöster.Text = "Bilgileri Göster";
+            this.btnBilgileriGöster.UseVisualStyleBackColor = true;
+            this.btnBilgileriGöster.Click += new System.EventHandler(this.btnBilgileriGoster_Click);
+            // 
+            // MarkatextBox
+            // 
+            this.MarkatextBox.Location = new System.Drawing.Point(256, 89);
+            this.MarkatextBox.Multiline = true;
+            this.MarkatextBox.Name = "MarkatextBox";
+            this.MarkatextBox.Size = new System.Drawing.Size(98, 22);
+            this.MarkatextBox.TabIndex = 7;
+            // 
+            // ModeltextBox
+            // 
+            this.ModeltextBox.Location = new System.Drawing.Point(256, 130);
+            this.ModeltextBox.Name = "ModeltextBox";
+            this.ModeltextBox.Size = new System.Drawing.Size(98, 22);
+            this.ModeltextBox.TabIndex = 8;
+            // 
+            // RenktextBox
+            // 
+            this.RenktextBox.Location = new System.Drawing.Point(256, 174);
+            this.RenktextBox.Name = "RenktextBox";
+            this.RenktextBox.Size = new System.Drawing.Size(98, 22);
+            this.RenktextBox.TabIndex = 9;
+            // 
+            // KapıSayısıtextBox
+            // 
+            this.KapıSayısıtextBox.Location = new System.Drawing.Point(256, 223);
+            this.KapıSayısıtextBox.Name = "KapıSayısıtextBox";
+            this.KapıSayısıtextBox.Size = new System.Drawing.Size(98, 22);
+            this.KapıSayısıtextBox.TabIndex = 10;
+            // 
+            // PencereSayısıtextBox
+            // 
+            this.PencereSayısıtextBox.Location = new System.Drawing.Point(256, 272);
+            this.PencereSayısıtextBox.Name = "PencereSayısıtextBox";
+            this.PencereSayısıtextBox.Size = new System.Drawing.Size(98, 22);
+            this.PencereSayısıtextBox.TabIndex = 11;
+            // 
+            // YakıttextBox
+            // 
+            this.YakıttextBox.Location = new System.Drawing.Point(256, 313);
+            this.YakıttextBox.Name = "YakıttextBox";
+            this.YakıttextBox.Size = new System.Drawing.Size(98, 22);
+            this.YakıttextBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.YakıttextBox);
+            this.Controls.Add(this.PencereSayısıtextBox);
+            this.Controls.Add(this.KapıSayısıtextBox);
+            this.Controls.Add(this.RenktextBox);
+            this.Controls.Add(this.ModeltextBox);
+            this.Controls.Add(this.MarkatextBox);
+            this.Controls.Add(this.btnBilgileriGöster);
+            this.Controls.Add(this.Yakıtlbl);
+            this.Controls.Add(this.PencereSayısılbl);
+            this.Controls.Add(this.KapıSayısılbl);
+            this.Controls.Add(this.Renklbl);
+            this.Controls.Add(this.Modellbl);
+            this.Controls.Add(this.Markalbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Markalbl; 
+        private System.Windows.Forms.Label Modellbl;
+        private System.Windows.Forms.Label Renklbl;
+        private System.Windows.Forms.Label KapıSayısılbl;
+        private System.Windows.Forms.Label PencereSayısılbl;
+        private System.Windows.Forms.Label Yakıtlbl;
+        private System.Windows.Forms.Button btnBilgileriGöster;
+        private System.Windows.Forms.TextBox MarkatextBox;
+        private System.Windows.Forms.TextBox ModeltextBox;
+        private System.Windows.Forms.TextBox RenktextBox;
+        private System.Windows.Forms.TextBox KapıSayısıtextBox;
+        private System.Windows.Forms.TextBox PencereSayısıtextBox;
+        private System.Windows.Forms.TextBox YakıttextBox;
+    }
+}
+
---
+++ ArabaBilgiForm/ArabaBilgiForm/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ArabaBilgiForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnBilgileriGoster_Click(object sender, EventArgs e)
+        {
+            // TextBox'lardan alınan veriler
+            string marka = MarkatextBox.Text;
+            string model = ModeltextBox.Text;
+            string renk = RenktextBox.Text;
+            string kapiSayisi = KapıSayısıtextBox.Text;
+            string pencereSayisi = PencereSayısıtextBox.Text;
+            string yakit = YakıttextBox.Text;
+
+            // Verileri MessageBox ile gösterme
+            MessageBox.Show(
+                $"Marka: {marka}\n" +
+                $"Model: {model}\n" +
+                $"Renk: {renk}\n" +
+                $"Kapı Sayısı: {kapiSayisi}\n" +
+                $"Pencere Sayısı: {pencereSayisi}\n" +
+                $"100 km’de Yaktığı Yakıt: {yakit} Litre",
+                "Araba Bilgileri",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
